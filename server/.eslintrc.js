db8cf329acd4c91ae131fe0bb597a118da04dc5f
Brian Purnell <purnellbp@gmail.com>
2021-10-10 19:35:50 -0500

    setup of server

---
+++ server/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
